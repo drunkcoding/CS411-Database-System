fc2770eec3d53ca5b648a39bc24a55c5bca27451
Leyang(Nick) Xue <xue.thinkingreed@gmail.com>
2020-03-07 20:19:13 +0800

    add imitial date

--- gunviolence/forms.py
+++ gunviolence/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+import datetime
 
 class SQLForm(forms.Form):
     query = forms.CharField(label='rawsql', max_length=1024)
 
 class DateRangeForm(forms.Form):
-    from_date = forms.DateField()
-    to_date = forms.DateField()
+    from_date = forms.DateField(initial=datetime.date(year=2014,month=4,day=11))
+    to_date = forms.DateField(initial=datetime.date(year=2014,month=4,day=12))
